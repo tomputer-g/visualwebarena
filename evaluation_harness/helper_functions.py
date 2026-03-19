071373b71aa3a3b18d194061c00b3ad9cb32fb2c
Tom Gao <zimingg@andrew.cmu.edu>
2026-03-19 16:49:39 -0400

    Separate evaluation harness calls with different key/url; Removed unused function signatures

--- evaluation_harness/helper_functions.py
+++ evaluation_harness/helper_functions.py
@@ -17,6 +17,7 @@ from browser_env.env_config import (
 )
 from llms.providers.openai_utils import (
     generate_from_openai_chat_completion,
+    generate_from_openai_chat_completion_evaluation_harness,
 )
 
 
@@ -592,7 +593,7 @@ def llm_fuzzy_match(pred: str, reference: str, question: str) -> float:
         {"role": "user", "content": message},
     ]
 
-    response = generate_from_openai_chat_completion(
+    response = generate_from_openai_chat_completion_evaluation_harness(
         model="gpt-4-1106-preview",
         messages=messages,
         temperature=0,
@@ -627,7 +628,7 @@ def llm_ua_match(pred: str, reference: str, question: str) -> float:
         {"role": "user", "content": message},
     ]
 
-    response = generate_from_openai_chat_completion(
+    response = generate_from_openai_chat_completion_evaluation_harness(
         model="gpt-4-1106-preview",
         messages=messages,
         temperature=0,
