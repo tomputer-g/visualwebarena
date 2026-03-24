941cb8c6a9327d728a32a4c8448bc7167a20e3c1
Ziming Gao <zgao9@w003.ib.bridges2.psc.edu>
2026-03-23 20:00:16 -0400

    Eval setup on PSC, vanilla Qwen

--- vwa.sh
+++ vwa.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-cd /ocean/projects/cis260044p/shared/src/visualwebarena/
 
 python run.py \
    --instruction_path agent/prompts/jsons/p_som_cot_id_actree_3s.json \
-   --test_start_idx 0   --test_end_idx 10  \
-   --result_dir ./results/  \
-   --test_config_base_dir=config_files/vwa/test_classifieds  \
-   --model Qwen/Qwen2.5-VL-3B-Instruct   \
+   --test_start_idx 341   --test_end_idx 1000  \
+   --result_dir ./results_all_shopping_actree_qwen_baseline_mar23/  \
+   --test_config_base_dir=config_files/vwa/test_shopping  \
+   --model Qwen/Qwen2.5-VL-7B-Instruct   \
    --action_set_tag som  --observation_type image_som \
    --provider openai --max_obs_length 15360 --mode chat
